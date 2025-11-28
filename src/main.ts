237071f699304c065d409b3764aeb5af501e1911
gabriel-peruchi <gabriel-peruchi@hotmail.com>
2025-11-28 07:35:08 -0300

    chore: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { initializeTransactionalContext } from 'typeorm-transactional'
+import { LoggerFactory } from '@sharedModules/logger/factory/logger.factory'
 
 async function bootstrap() {
   initializeTransactionalContext()
-  const app = await NestFactory.create(AppModule)
+  const logger = LoggerFactory('application-main')
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  })
+  app.useLogger(logger)
   await app.listen(3000)
 }
+
 bootstrap()
